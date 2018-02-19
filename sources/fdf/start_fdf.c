a65fd6ba40ff4c7217bcc84a557a44d9b978e273
Mykola ODNOSUMOV <modnosum@e1r7p13.unit.ua>
2018-02-19 20:39:34 +0200

    Testing hooks

--- sources/fdf/start_fdf.c
+++ sources/fdf/start_fdf.c
@@ -6,7 +6,7 @@
 /*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 17:11:17 by modnosum          #+#    #+#             */
-/*   Updated: 2018/02/19 17:11:17 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/02/19 19:49:24 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 void					start_fdf(t_fdf *fdf)
 {
 	mlx_key_hook(fdf->window, &keyboard_hook, fdf);
-	mlx_mouse_hook(fdf->window, &mouse_hook, fdf);
+	mlx_mouse_hook(fdf->window, &mouse_release_hook, fdf);
+	/* mlx_hook(fdf->window, MOTION_NOTIFY, MOUSE_MOTION_MASK, */
+	/* 		 &mouse_motion_hook, fdf); */
+//	mlx_expose_hook(fdf->window, &expose_hook, fdf);
+	/* draw_plain(fdf->mlx, fdf->image, fdf->plain); */
+	/* redraw(fdf); */
 	mlx_loop(fdf->mlx);
 }

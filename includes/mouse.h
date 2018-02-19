197aafc0bdfd34238320a43ac3954bc4c393d599
Mykola ODNOSUMOV <modnosum@e1r7p13.unit.ua>
2018-02-19 20:37:24 +0200

    Add mouse header

---
+++ includes/mouse.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/19 19:49:53 by modnosum          #+#    #+#             */
+/*   Updated: 2018/02/19 19:52:24 by modnosum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MOUSE_H
+# define MOUSE_H
+
+# if defined(__APPLE__)
+
+#  define MOUSE_LEFT 1
+#  define MOUSE_RIGHT 2
+#  define MOUSE_MIDDLE 3
+#  define MOUSE_SCROLL_UP 4
+#  define MOUSE_SCROLL_DOWN 5
+#  define MOUSE_SCROLL_LEFT 6
+#  define MOUSE_SCROLL_RIGHT 7
+
+# endif
+
+#endif

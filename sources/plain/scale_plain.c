561ba417f3bbf8eb230f25f1a7601809cbc15d0d
Mykola Odnosumov <modnosum@gmail.com>
2018-02-21 20:42:00 +0200

    Fix z not scaling

--- sources/plain/scale_plain.c
+++ sources/plain/scale_plain.c
@@ -6,7 +6,7 @@
 /*   By: modnosum <modnosum@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 23:21:02 by modnosum          #+#    #+#             */
-/*   Updated: 2018/02/21 01:08:51 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/02/21 20:37:53 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void					scale_plain(t_plain *pln, int btn)
 	{
 		pln->sx += ZOOM_IN(btn) ? (1) : (-1);
 		pln->sy += ZOOM_IN(btn) ? (1) : (-1);
+		pln->sz += ZOOM_IN(btn) ? (1) : (-1);
 	}
 	else if (SCALE_X(btn))
 		pln->sx += SCALE_X_P(btn) ? (1) : (-1);
